6a9eebe02f90e0d6aabec90eb7e710f7703baed6
alfred <vuyanimko616@gmail.com>
2017-06-01 14:27:16 +0200

    import unused pages

---
+++ src/pages/about-popover/about-popover.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+import { App, NavController, ModalController, ViewController } from 'ionic-angular';
+
+
+@Component({
+  template: `
+    <ion-list>
+      <button ion-item (click)="close('https://www.tfg.co.za/find-a-store')">More</button>
+      <button ion-item (click)="close('https://www.tfg.co.za/help')">Get help</button>
+    </ion-list>
+  `
+})
+export class PopoverPage {
+
+  constructor(
+    public viewCtrl: ViewController,
+    public navCtrl: NavController,
+    public app: App,
+    public modalCtrl: ModalController
+  ) { }
+
+  support() {
+    this.app.getRootNav().push('SupportPage');
+    this.viewCtrl.dismiss();
+  }
+
+  close(url: string) {
+    window.open(url, '_blank');
+    this.viewCtrl.dismiss();
+  }
+}
---
+++ src/pages/login/login.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { NavController } from 'ionic-angular';
+
+import { UserData } from '../../providers/user-data';
+
+import { SignupPage } from '../signup/signup';
+import { SchedulePage } from '../schedule/schedule';
+
+
+@Component({
+  selector: 'page-user',
+  templateUrl: 'login.html'
+})
+export class LoginPage {
+  login: {username?: string, password?: string} = {};
+  submitted = false;
+
+  constructor(public navCtrl: NavController, public userData: UserData) { }
+
+  onLogin(form: NgForm) {
+    this.submitted = true;
+
+    if (form.valid) {
+      this.userData.login(this.login.username);
+      this.navCtrl.push(SchedulePage);
+    }
+  }
+
+  onSignup() {
+    this.navCtrl.push(SignupPage);
+  }
+}
