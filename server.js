98ce946d8109062f29d0a63658756cbb8faa7878
fatimafenton <fatimafenton92@gmail.com>
2017-06-02 22:12:13 +0200

    connecting azure database

--- server.js
+++ server.js
@@ -5,3 +5,31 @@ mobileApp.tables.add('TodoItem'); // Create a table for 'TodoItem' with default 
 
 app.use(mobileApp);
 app.listen(process.env.PORT || 3000);
+
+var Connection = require('tedious').Connection;
+var Request = require('tedious').Request;
+
+
+// Create connection to database
+var config = {
+  userName: 'sihle', // update me
+  password: '@thando1982', // update me
+  server: 'sihle-1.database.windows.net', // update me
+  options: {
+      database: 'sihle-1' //update me
+  }
+}
+
+var connection = new Connection(config);
+
+// Attempt to connect and execute queries if connection goes through
+connection.on('connect', function(err) {
+    if (err) {
+        console.log(err)
+    }
+    else{
+        updateInDatabase()
+    }
+});
+
+connection.execSql(request);
